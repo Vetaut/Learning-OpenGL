2a88c22c5bf7704d4671a9803dee83b858f1117d
Vetaut <olneywill@msn.com>
2019-08-15 20:39:18 -0400

    Transformations

--- Window/Window/main.cpp
+++ Window/Window/main.cpp
@@ -11,6 +11,9 @@
 #include <math.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "stb_image.h"
 #include "shader.h"
@@ -178,6 +181,14 @@ int main() {
         
         // ... Drawing ...
         ourShader.use();
+        ourShader.setFloat("mixture", 0.2f);
+        
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+        
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
         
         // render the triangles
         glActiveTexture(GL_TEXTURE0);
@@ -186,6 +197,14 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, texture2);
         
         glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        float scaleAmount = sin(glfwGetTime());
+        trans = glm::mat4(1.0f);
+        trans = glm::scale(trans, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+        
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &trans[0][0]);
+        
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         // check and call all events and swap the buffers
